faa5e473011058d067e1c00e9386691389412376
4FunOnly <76902001+4FunOnly@users.noreply.github.com>
2022-07-07 16:59:19 +0700

    Deleting old files

--- parameter.py
+++
@@ -1,9 +0,0 @@
-#Site Query
-siteQuery= 'https://linkedin.com/jobs/search/?f_E=2&keywords=information%20systems'
-
-#hasil
-hasil='hasil.csv'
-
-#login Linkedin
-email = "helmifachry15@gmail.com"
-password ="kodemasuk"
